938dfb78ed8889af3e388bbcc3d4c7cb903cbbb1
ZhiCun <island205@gmail.com>
2012-07-16 14:46:14 +0800

    format code

--- src/canvas/vango.js
+++ src/canvas/vango.js
@@ -1,11 +1,9 @@
 /*
  *vango.js a light lib extending from canvas API
- */;
-
-
+ */
 /*
  * lang
- */
+ */;
 (function () {
     /*
      *https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
@@ -85,49 +83,49 @@
 })(this, function () {
 
     var __hasProp = Object.prototype.hasOwnProperty,
-    DOC=document,
-    vangoprop=Vango.prototype,
-  	__animate;
-	
-		__animate = (function() {
-  	  var requestAnimationFrame;
-  	  requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback) {
-  	    setTimeout(function() {
-  	      callback(new Date());
-  	    }, 1000 / 60);
-  	  };
-  	  animate = function(duration, callback) {
-  	    var finished, startTime, step;
-  	    startTime = new Date();
-  	    finished = -1;
-  	    step = function(timestemp) {
-  	      var progress;
-  	      progress = timestemp - startTime;
-  	      if (progress >= duration) {
-  	        callback(finished);
-  	        return;
-  	      }
-  	      callback(progress);
-  	      return requestAnimationFrame(step);
-  	    };
-  	    return requestAnimationFrame(step);
-  	  };
-  	  return animate;
-  	})();
-	
+        DOC = document,
+        vangoprop = Vango.prototype,
+        __animate;
+
+    __animate = (function () {
+        var requestAnimationFrame;
+        requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
+            setTimeout(function () {
+                callback(new Date());
+            }, 1000 / 60);
+        };
+        animate = function (duration, callback) {
+            var finished, startTime, step;
+            startTime = new Date();
+            finished = -1;
+            step = function (timestemp) {
+                var progress;
+                progress = timestemp - startTime;
+                if (progress >= duration) {
+                    callback(finished);
+                    return;
+                }
+                callback(progress);
+                return requestAnimationFrame(step);
+            };
+            return requestAnimationFrame(step);
+        };
+        return animate;
+    })();
+
 
     /*
      * Vango instance constructor
      */
     function Vango(container, width, height) {
         var cvs;
-        if(container == null){
+        if (container == null) {
             return;
         }
-        cvs=this.canvas=DOC.createElementByTagName("canvas");
-        cvs.width=width;
-        cvs.height=height;
-        this.context=cvs.getContext("2d");
+        cvs = this.canvas = DOC.createElementByTagName("canvas");
+        cvs.width = width;
+        cvs.height = height;
+        this.context = cvs.getContext("2d");
         container.appendChild(cvs);
     }
 
@@ -148,19 +146,19 @@
         return this.canvas.style[property];
     });
 
-    ["getContext", "toDataURL"].forEach(function(method){
-        vangoprop[method]=function(){
-            var cvs=this.canvas;
-            return cvs[method].apply(cvs,arguments);
+    ["getContext", "toDataURL"].forEach(function (method) {
+        vangoprop[method] = function () {
+            var cvs = this.canvas;
+            return cvs[method].apply(cvs, arguments);
         }
     });
 
-	
-	/*
-	 * Context
-	 */
 
-	/*
+    /*
+     * Context
+     */
+
+    /*
 		attrName					attrValue				option
 		
 		canvas						--						readonly
@@ -184,129 +182,118 @@
 									{linearGradient}
 									{radialGradient}
 									{canvasPattern}
-	*/	
-	Vango.style=function(key,value){
-		this.context[key]=value;
-	};
+	*/
+    Vango.style = function (key, value) {
+        this.context[key] = value;
+    };
 
-	Vango.style=__overloadGetterSetter.call(Vango.style,function(key){
-		return this.context[key];	
-	});
-	
-	/*
-	 * void mehtod
-	 * return tihs
-	 */
+    Vango.style = __overloadGetterSetter.call(Vango.style, function (key) {
+        return this.context[key];
+    });
 
-	[
-	/*
-	 * path method
-	 */
-	  "beginPath","closePath",
-	  "fill","stroke","clip",
-	  "moveTo",
-	  "lineTo","arc","arcTo","bezierCurveTo","quadraticCurveTo","rect",
-	  /*
-	   * rectangles
-	   */
-	  "clearRect","fillRect","strokeRect",
-	  /*
-	   * text
-	   */
-	  "fillText","strokeText",
-	  /*
-	   * image drawing
-	   */
-	  "drawImage",
-	  /*
-	   * pixel manipulation
-	   */
-	  "putImageData",
-	  /*
-	   * 2D Context
-	   */
-	  "save","restore",
-	  /*
-	   * transform
-	   */
-	  "scale","rotate","translate","transform","setTransform"
-	].forEach(function(method){
-		 var ctx=this.context;
-		 ctx[method].apply(ctx,arguments);
-		 return this;
-	});
-	
+    /*
+     * void mehtod
+     * return tihs
+     */
 
-	/*
-	 * return original returns
-	 */ 
-	[
-	  /*
-	   * path
-	   */
-	  "isPointInPath",
-	  /*
-	   * text
-	   * measureText Interface
-	   * width	{float}	readonly
-	   */
-	  "measureText",
-	  /*
-	   * pixel manipulation
-	   * imageData	interface
-	   * 	width	unsigned long	readyonly
-	   * 	height	unsigned long	readyonly
-	   * 	data	CanvasPixelArray	readyonly
-	   * CanvasPixelArray interface
-	   * 	length	unsigned	readyonly
-	   */
-	  "createImageData","getImageData",
-	  /*
-	   * color style & shadow
-	   * CanvasGradient interface
-	   * 	void	addColorStop(float offset,string color)
-	   */
-	  "createLinearGradient","createRadialGradient","createPattern"
-	],forEach(function(method){
-		 var ctx=this.context;
-		 return ctx[method].apply(ctx,arguments);
-	});
+    [
+    /*
+     * path method
+     */
+    "beginPath", "closePath", "fill", "stroke", "clip", "moveTo", "lineTo", "arc", "arcTo", "bezierCurveTo", "quadraticCurveTo", "rect",
+    /*
+     * rectangles
+     */
+    "clearRect", "fillRect", "strokeRect",
+    /*
+     * text
+     */
+    "fillText", "strokeText",
+    /*
+     * image drawing
+     */
+    "drawImage",
+    /*
+     * pixel manipulation
+     */
+    "putImageData",
+    /*
+     * 2D Context
+     */
+    "save", "restore",
+    /*
+     * transform
+     */
+    "scale", "rotate", "translate", "transform", "setTransform"].forEach(function (method) {
+        var ctx = this.context;
+        ctx[method].apply(ctx, arguments);
+        return this;
+    });
 
-	
 
-	/*
-	 * extend Canvas
-	 */	
-	Vango.extend=function(name,method){
-		vangoprop[name]=method;
-	}
-	Vango.extend=__overloadGetterSetter.call(Vango.extend);
-    
-	
-	Vango.extend("extend",__overloadGetterSetter.call(function(name,method){
-		  this[name]=method;
-	}));
-	
-	/*
-	 * extend context
-	 */
-	Vango.extend({
-		/*
-		 * graph extends
-		 */
-		line:function(sx,sy,ex,ey){
-		},
-		cicle:function(x,y,radius){
-		},
-		rect:function(x,y,width,height){
-		},
-		ellipse:function(x,y,radiusX,radiusY){
-		},
-		polygon:function(n, x, y, radius, angle){
-		},
-		sector:function(x,y,radius,startAngle,endAngle){
-		},
-		/*@params pathString {String}	path string in SVG format.	"M10,20L30,40"
+    /*
+     * return original returns
+     */
+    [
+    /*
+     * path
+     */
+    "isPointInPath",
+    /*
+     * text
+     * measureText Interface
+     * width	{float}	readonly
+     */
+    "measureText",
+    /*
+     * pixel manipulation
+     * imageData	interface
+     * 	width	unsigned long	readyonly
+     * 	height	unsigned long	readyonly
+     * 	data	CanvasPixelArray	readyonly
+     * CanvasPixelArray interface
+     * 	length	unsigned	readyonly
+     */
+    "createImageData", "getImageData",
+    /*
+     * color style & shadow
+     * CanvasGradient interface
+     * 	void	addColorStop(float offset,string color)
+     */
+    "createLinearGradient", "createRadialGradient", "createPattern"], forEach(function (method) {
+        var ctx = this.context;
+        return ctx[method].apply(ctx, arguments);
+    });
+
+
+
+    /*
+     * extend Canvas
+     */
+    Vango.extend = function (name, method) {
+        vangoprop[name] = method;
+    }
+    Vango.extend = __overloadGetterSetter.call(Vango.extend);
+
+
+    Vango.extend("extend", __overloadGetterSetter.call(function (name, method) {
+        this[name] = method;
+    }));
+
+    /*
+     * extend context
+     */
+    Vango.extend({
+        /*
+         * graph extends
+         */
+        line: function (sx, sy, ex, ey) {},
+        cicle: function (x, y, radius) {},
+        rect: function (x, y, width, height) {},
+        ellipse: function (x, y, radiusX, radiusY) {},
+        polygon: function (n, x, y, radius, angle) {},
+        sector: function (x, y, radius, startAngle, endAngle) {},
+        /*@params pathString {String}	path string in SVG format.	"M10,20L30,40"
 		Command	Name								Parameters
 		M		moveto								(x y)+
 		Z		closepath							(none)
@@ -320,33 +307,33 @@
 		A		elliptical arc						(rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
 		R		Catmull-Rom curveto*				x1 y1 (x y)+
 		*/
-	  path:function(pathString){},
-		image:function(src,x,y){},
-		text:function(x,y,text){},
+        path: function (pathString) {},
+        image: function (src, x, y) {},
+        text: function (x, y, text) {},
 
-		/*
-		 * transform extends
-		 */
-		shear:function(kx,ky){},
-		rotateAbout:function(x,y,theta){}
-	});
-
-	
-	/*
-	 * native events adapter
-	 */
-	Vango.extend({
-		on:function(type,listener){},
-		off:function(type,listener){}		
-	});
-
-	/*
-	 * animate
-	 */
-	vangoprop.animate=__animate;
+        /*
+         * transform extends
+         */
+        shear: function (kx, ky) {},
+        rotateAbout: function (x, y, theta) {}
+    });
 
 
-	/*
+    /*
+     * native events adapter
+     */
+    Vango.extend({
+        on: function (type, listener) {},
+        off: function (type, listener) {}
+    });
+
+    /*
+     * animate
+     */
+    vangoprop.animate = __animate;
+
+
+    /*
      * util
      */
     function __extend(target, source) {
