f38afaaf23600c85152779c43c2fca80b5ac3459
xinwei.wang <wxwdesign@gmail.com>
2012-07-25 14:35:25 +0800

    update DPChart.getColors(c); and let it create more different colors

--- src/svg/DPChart.bar.js
+++ src/svg/DPChart.bar.js
@@ -1,8 +1,4 @@
-(function () {
-	var colors = DPChart.getColors();
-	
-	console.log(colors);
-	
+(function () {	
 	function VerticalBar(paper, x, y, ox, oy, w, h, color, d) {
 		var path, rect = {}, bar;
 		
@@ -39,8 +35,8 @@
 			var xAxis = this.axises.x,
 				yAxis = this.axises.y;
 				
-			var xOrigin = xAxis.getOrigin();
-			var yOrigin = yAxis.getOrigin();
+			var colors = DPChart.getColors(series.length);
+			console.log(colors);
 			
 			// console.log('data series elements count: ', series.length);
 			
@@ -54,18 +50,24 @@
 				// console.log('data', i, data);
 				// console.log(posX, posY);
 				
+				//width and height of bar
+				width = yAxis.getOrigin().x - posX;
+				height = xAxis.getOrigin().y - posY;
+				
 				/**position offset between two bar elements*/
+				//offset between current and next element
 				if (series[i + 1]) {
 				    posOffset.x = xAxis.options.tickWidth||xAxis.getX(i + 1)-posX;
 				    posOffset.y = xAxis.options.tickHeight||yAxis.getY(i + 1)-posY;
-				}else{
+				//offset between current and previous element
+				}else if(series[i-1]){
 					posOffset.x = xAxis.options.tickWidth||posX-xAxis.getX(i-1);
 				    posOffset.y = xAxis.options.tickHeight||posY-yAxis.getY(i-1);
-				}
-				
-				//width and height of bar
-				width = yOrigin.x - posX;
-				height = xOrigin.y - posY;
+				//just one element
+				}else{
+					posOffset.x = xAxis.options.tickWidth||0.382*height;
+				    posOffset.y = xAxis.options.tickHeight||0.382*width;
+				}				
 				
 				// VerticalBar.call(this, posX, posY, posOffset.x, posOffset.y, width, height);
 				VerticalBar(this.raphael, posX, posY, posOffset.x, posOffset.y, width, height, colors[i], data.data);
